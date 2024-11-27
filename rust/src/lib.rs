ecaac4f7a4659e20b257cd4402581825ca4c5a43
Steph Kraemer <purple.stephyr@proton.me>
2024-11-26 21:58:47 -0500

    Move to mouse

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,9 @@
+use godot::classes::CharacterBody2D;
+use godot::classes::ICharacterBody2D;
+use godot::classes::InputEvent;
+use godot::classes::InputEventMouseButton;
+use godot::global::MouseButton;
 use godot::prelude::*;
-use godot::classes::Sprite2D;
-use godot::classes::ISprite2D;
 
 struct MyExtension;
 
@@ -8,32 +11,50 @@ struct MyExtension;
 unsafe impl ExtensionLibrary for MyExtension {}
 
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base=CharacterBody2D)]
 struct Player {
     speed: f64,
-    angular_speed: f64,
+    destination: Option<Vector2>,
 
-    base: Base<Sprite2D>,
+    base: Base<CharacterBody2D>,
 }
 
 #[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
+impl ICharacterBody2D for Player {
+    fn init(base: Base<CharacterBody2D>) -> Self {
         godot_print!("Hello, world!");
 
         Self {
             speed: 400.0,
-            angular_speed: std::f64::consts::PI,
+            destination: None,
             base,
         }
     }
 
-    fn physics_process(&mut self, delta: f64) {
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
+    fn input(&mut self, event: Gd<InputEvent>) {
+        if let Ok(event) = event.try_cast::<InputEventMouseButton>() {
+            if event.is_pressed() && event.get_button_index() == MouseButton::LEFT {
+                self.destination = Some(event.get_position());
+                godot_print!("Got left mouse button press {}, {}", self.destination.unwrap().x, self.destination.unwrap().y);
+            }
+        }
+    }
 
-        let rotation = self.base().get_rotation();
-        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
-        self.base_mut().translate(velocity * delta as f32);
+    fn physics_process(&mut self, _delta: f64) {
+        if let Some(destination) = self.destination {
+            match self.base().get_position().distance_to(destination) {
+                d if d > 10.0 => {
+                    let velocity = self.base().get_position().direction_to(destination) * self.speed as f32;
+                    self.base_mut().set_velocity(velocity);
+                    self.base_mut().move_and_slide();
+                }
+                d if d > 0.0 => {
+                    self.base_mut().set_position(destination);
+                }
+                _ => {
+                    self.destination = None;
+                }
+            }
+        }
     }
 }
